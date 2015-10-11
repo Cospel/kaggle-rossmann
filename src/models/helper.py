93fe2e315aee7c65a8c410a34012de1137b6367e
MichalLukac <cospelthetraceur@gmail.com>
2015-10-10 22:00:19 -0700

    update for loading simple data

--- src/models/helper.py
+++ src/models/helper.py
@@ -41,13 +41,9 @@ def get_training_dataset_simple(datam):
     @param n_prev: array of length of sequences
     @return d
     """
-    docX, docY = [], []
-    tempX, tempY = get_data_sequence(datam, n_prev=0)
-    for sequence, label in zip(tempX, tempY):
-        docX.append(sequence)
-        docY.append(label)
-
-    return np.array(docX), np.array(docY)
+    docX = datam.as_matrix(columns=columns)
+    docY = datam['Sales'].tolist()
+    return docX, np.array(docY)
 
 def get_data_sequence(datam, n_prev=1):
     """
@@ -57,39 +53,10 @@ def get_data_sequence(datam, n_prev=1):
     """
     docX, docY = [], []
     lendata = len(datam)
-    if n_prev == 0:
-        for i in xrange(lendata-n_prev):
-            #print(str(i)+'/'+str(lendata))
-            # this is simple data for non rnn models
-            docX.append(datam[columns].iloc[i].as_matrix())
-            docY.append(datam['Sales'].iloc[i])
-    else:
-        for i in xrange(lendata-n_prev):
-            #print(str(i)+'/'+str(lendata))
-            # this are data sequences for rnn models
-            docX.append(datam[columns].iloc[i:i+n_prev].as_matrix())
-            docY.append(datam['Sales'].iloc[i+n_prev])
-    return (docX, docY)
-
-def get_test_data_sequence(datam, n_prev=1):
-    """
-    @param datam : pandas dataframe
-    @param n_prev: number of previous examples/timesteps for RNN
-    @return: (X,Y) divided data (lists of numpy array)
-    """
-    docX = []
-    lendata = len(datam)
-    if n_prev == 0:
-        for i in xrange(lendata-n_prev):
-            #print(str(i)+'/'+str(lendata))
-            # this is simple data for non rnn models
-            docX.append(datam[columns].iloc[i].as_matrix())
-    else:
-        for i in xrange(lendata-n_prev):
-            #print(str(i)+'/'+str(lendata))
-            # this are data sequences for rnn models
-            docX.append(datam[columns].iloc[i:i+n_prev].as_matrix())
-    return docX
+    for i in xrange(lendata-n_prev):
+        docX.append(datam[columns].iloc[i:i+n_prev].as_matrix())
+        docY.append(datam['Sales'].iloc[i+n_prev])
+    return docY
 
 def get_test_dataset_simple(datam):
     """
@@ -97,8 +64,8 @@ def get_test_dataset_simple(datam):
     @param n_prev: array of length of sequences
     @return d
     """
-    tempX = get_test_data_sequence(datam, n_prev=0)
-    return np.array(tempX)
+    docX = datam.as_matrix(columns=columns)
+    return docX
 
 def train_test_split(df, test_size=0.1):
     """
