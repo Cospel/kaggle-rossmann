52b28f54103573e527548e0b5956410136ea9cff
MichalLukac <cospelthetraceur@gmail.com>
2015-10-14 14:10:13 -0700

    update

--- src/models/lstm_simple.py
+++ src/models/lstm_simple.py
@@ -18,6 +18,16 @@ import os.path
 
 from helper import *
 
+# which columns we will use for model
+columns = ['Store', 'CompetitionDistance', 'Promo2', 'Open', 'Promo',
+           'StateHoliday_a','StateHoliday_b', 'StateHoliday_c','StateHoliday_0',
+           'Assortment_a','Assortment_b','Assortment_c','Assortment_nan',
+           'StoreType_a','StoreType_b','StoreType_c','StoreType_d','StoreType_nan',
+           'DayOfWeek_1.0','DayOfWeek_2.0','DayOfWeek_3.0','DayOfWeek_4.0','DayOfWeek_5.0','DayOfWeek_6.0','DayOfWeek_7.0',
+           'WeekOfMonth_1.0','WeekOfMonth_2.0','WeekOfMonth_3.0','WeekOfMonth_4.0','WeekOfMonth_5.0','WeekOfMonth_6.0',
+           'Month_1.0','Month_2.0','Month_3.0','Month_4.0','Month_5.0','Month_6.0','Month_7.0','Month_8.0','Month_9.0','Month_10.0','Month_11.0','Month_12.0',
+           'SchoolHoliday']
+
 print('Loading data ...')
 data_dir = '../../data/'
 hdf = HDFStore(data_dir + 'data.h5')
@@ -27,8 +37,8 @@ data_train['Date'] = pd.to_datetime(data_train.Date)
 data_train = data_train.ix[pd.to_datetime(data_train.Date).order().index]
 (DataTr, DataTe) = train_test_split(data_train,0.00)
 
-in_neurons = 12
-hidden_neurons = 250
+in_neurons = len(columns)
+hidden_neurons = 300
 hidden_neurons_2 = 300
 out_neurons = 1
 nb_epoch = 10
@@ -58,7 +68,7 @@ for epoch in range(10):
         print ('Epoch:', epoch,' Store:', store)
         data = DataTr[DataTr.Store == store]
         # get max 10 day sequence window
-        x, y = get_data_sequence(data,n_prev=7)
+        x, y = get_data_sequence(data,columns,n_prev=7)
         print ('Evaluation loss before:', model.evaluate(x,y,verbose=2))
         model.fit(x, y, validation_split=0.00, batch_size=4,shuffle=True,nb_epoch=2,verbose=2)
         model.save_weights('lstm_simple_weights', overwrite=True)
